930b25a8aceb2c409a85069b3b339e15a33c3450
Roy Revelt <roy@codsen.com>
2021-12-10 00:25:41 +0000

    feat: update dependencies, add more tests, improve the setup

--- index.js
+++ index.js
@@ -1,56 +1,38 @@
-'use strict';
-
-const postcss = require('postcss');
-const fs = require('fs');
+const { readFileSync } = require("fs");
+const path = require("path");
 
 /**
- * ParseImportPath
- * @param  {[type]} path [description]
- * @return {[type]}      [description]
+ * @type {import('postcss').PluginCreator}
  */
-function parseImportPath(path) {
-    const matches = path.trim().match(/^['"](.+?)['"](.*)/);
-    // const modes = matches[2].trim().split(' ');
-    const fragment = matches[1];
-    return fragment;
-}
+module.exports = () => {
+  return {
+    AtRule(node) {
+      if (node.name === "import") {
+        if (
+          !node.params ||
+          typeof node.params !== "string" ||
+          node.params.length < 3
+        ) {
+          return;
+        }
 
-/**
- * ReadFile
- * @param  {[type]} file [description]
- * @return {[type]}      [description]
- */
-function readFile(file) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(file, 'utf-8', (err, contents) => {
-            if (err) {
-                return reject(err);
-            }
-            return resolve(contents);
-        });
-    });
-}
+        let resolvedPath = path.resolve(
+          node.params.slice(
+            `'"`.includes(node.params[0]) ? 1 : 0,
+            `'"`.includes(node.params[~-node.params.length]) ? -1 : undefined
+          )
+        );
+        let replacement;
+        try {
+          replacement = readFileSync(resolvedPath, "utf8");
+        } catch (error) {
+          throw node.error(`error reading file:\n${resolvedPath}`);
+        }
+        node.replaceWith(`${node.raws.before}${replacement}`);
+      }
+    },
+    postcssPlugin: "ppostcss-nested-import"
+  };
+};
 
-/**
- * PostCSS Nested Import Plugin
- * @type {[type]}
- */
-module.exports = postcss.plugin('postcss-nested-import', () => {
-    return root => {
-        return new Promise(resolve => {
-            let prom = Promise.resolve();
-            root.walkAtRules('import', importAtRule => {
-                if (importAtRule.params) {
-                    const path = parseImportPath(importAtRule.params);
-                    if (path === null) {
-                        return;
-                    }
-                    prom = prom.then(() => readFile(path).then(fileContents =>
-                      importAtRule.replaceWith(fileContents)
-                    ));
-                }
-            });
-            resolve(prom);
-        });
-    };
-});
+module.exports.postcss = true;
